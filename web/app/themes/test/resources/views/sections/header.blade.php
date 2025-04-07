62b696fca0b8084e3df00f6136475cbd60c97ed9
SashaSolovey1 <solo160103@gmail.com>
2025-04-07 21:42:01 +0300

    header update

--- web/app/themes/test/resources/views/sections/header.blade.php
+++ web/app/themes/test/resources/views/sections/header.blade.php
@@ -28,31 +28,25 @@
       </nav>
     @endif
 
-    <div class="lg:hidden flex items-center gap-4">
-      <button @click="searchOpen = !searchOpen">
-        <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M11.6562 0.65625C7.6947 0.65625 4.46875 3.8822 4.46875 7.84375C4.46875 9.56482 5.07239 11.1427 6.08594 12.3809L0.358398 18.1084L1.3916 19.1416L7.11914 13.4141C8.3573 14.4276 9.93518 15.0312 11.6562 15.0312C15.6178 15.0312 18.8438 11.8053 18.8438 7.84375C18.8438 3.8822 15.6178 0.65625 11.6562 0.65625ZM11.6562 2.09375C14.8401 2.09375 17.4062 4.65991 17.4062 7.84375C17.4062 11.0276 14.8401 13.5938 11.6562 13.5938C8.47241 13.5938 5.90625 11.0276 5.90625 7.84375C5.90625 4.65991 8.47241 2.09375 11.6562 2.09375Z" fill="black"/>
-        </svg>
-      </button>
+  <div class="lg:hidden flex items-center gap-4">
+    <button @click="searchOpen = !searchOpen">
+      <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11.6562 0.65625C7.6947 0.65625 4.46875 3.8822 4.46875 7.84375C4.46875 9.56482 5.07239 11.1427 6.08594 12.3809L0.358398 18.1084L1.3916 19.1416L7.11914 13.4141C8.3573 14.4276 9.93518 15.0312 11.6562 15.0312C15.6178 15.0312 18.8438 11.8053 18.8438 7.84375C18.8438 3.8822 15.6178 0.65625 11.6562 0.65625ZM11.6562 2.09375C14.8401 2.09375 17.4062 4.65991 17.4062 7.84375C17.4062 11.0276 14.8401 13.5938 11.6562 13.5938C8.47241 13.5938 5.90625 11.0276 5.90625 7.84375C5.90625 4.65991 8.47241 2.09375 11.6562 2.09375Z" fill="black"/>
+      </svg>
+    </button>
 
-      <button @click="open = !open">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-          <path d="M4 6h16M4 12h16M4 18h16"></path>
-        </svg>
-      </button>
-    </div>
+    <button @click="open = !open">
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+        <path d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+    </button>
   </div>
+</div>
 
-  <div x-show="open" x-transition class="lg:hidden fixed top-0 left-0 right-0 bottom-0 h-full bg-white z-50 p-12 overflow-auto">
-    <div class="flex justify-end">
-      <button @click="open = false" aria-label="Close menu" class="text-3xl font-bold leading-none text-gray-800 hover:text-black">
-        &times;
-      </button>
-    </div>
-
+  <div x-show="open" x-transition class="lg:hidden container mt-4">
     {!! wp_nav_menu([
       'theme_location' => 'primary_navigation',
-      'menu_class' => 'flex flex-col gap-6 font-semibold text-lg text-black z-10',
+      'menu_class' => 'flex flex-col gap-4 font-semibold text-black',
       'echo' => false,
     ]) !!}
   </div>
